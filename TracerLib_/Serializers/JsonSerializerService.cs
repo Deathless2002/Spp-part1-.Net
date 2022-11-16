e2e320941f2f489127420f46770ecdc7e2f9b607
Egor Kudi <egorkudi2002@mail.ru>
2022-11-16 09:17:04 +0300

    json serialization added

---
+++ TracerLib_/Serializers/JsonSerializerService.cs
@@ -0,0 +1,19 @@
+﻿using Spp1._0.InfoModel;
+using System.Text.Json;
+using TracerLib.Interface;
+
+namespace TracerLib.Serializers
+{
+    public class JsonSerializerService : ISerializer
+    {
+        public string Serialize(TraceResult result)
+        {
+            JsonSerializerOptions jso = new()
+            {
+                WriteIndented = true
+            };
+
+            return JsonSerializer.Serialize(result, jso);
+        }
+    }
+}
